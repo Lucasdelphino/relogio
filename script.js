3fe15d3b86db600441771cbb872b73a44b961cb8
Lucasdelphino <80285529+Lucasdelphino@users.noreply.github.com>
2022-09-05 17:18:38 -0300

    relogio

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
+const milesimos = document.getElementById('milesimos');
+const data = document.getElementById('data');
+const mes = document.getElementById('mes');
 
 const relogio = setInterval(function time() {
     let dateToday = new Date();
+    let d = dateToday.getDate();
+    let m = dateToday.getMonth()+1;
+    let a = dateToday.getFullYear();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let s = dateToday.getSeconds();
+    let mill = dateToday.getMilliseconds();
+    
+    if (d <10) d = '0' + d
+
+    if (m <10) m = '0' + m
+
+    if (a <10) a = '0' + a
 
     if (hr < 10) hr = '0' + hr;
 
@@ -14,8 +27,15 @@ const relogio = setInterval(function time() {
 
     if (s < 10) s = '0' + s;
 
+    if (mill <10) mill = '0' + mill;
+
+ 
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = s;
+    milesimos.textContent = mill;
+    data.textContent = d;
+    mes.textContent = m
+    ano.textContent = a
 
 })
